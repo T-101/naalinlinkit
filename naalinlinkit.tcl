e9a5e8b5dd3e533d6d9e5fe96656cdc6c075c9f7
T-101 <t-101@t-101.org>
2013-09-12 00:49:56 +0300

    Added handler for youtube links of various forms

--- naalinlinkit.tcl
+++ naalinlinkit.tcl
@@ -28,6 +28,7 @@ if {[channel get $channel naalinlinkit] && [onchan $nick $channel]} {
 				if {[naalinlinkit::apinahandler $item [lindex $processline 2]]} { set urlfound true }
 				if {[naalinlinkit::imgurhandler $item [lindex $processline 2]]} { set urlfound true }
 				if {[naalinlinkit::kuvatonhandler $item [lindex $processline 2]]} { set urlfound true }
+				if {[naalinlinkit::youtubehandler $item [lindex $processline 2]]} { set urlfound true }
         	                if {[string match -nocase $item [lindex $processline 2]]} {set urlfound true}
 				if {$urlfound} {naalinlinkit::output $channel $processline}
 	               	        unset processline
@@ -64,6 +65,26 @@ if {[lindex [split [lindex [split $url "//"] 2] .] end-1] == "kuvaton"} {set yin
 if {[lindex [split [lindex [split $test "//"] 2] .] end-1] == "kuvaton"} {set yang [lindex [split [lindex [split $test "/"] end] .] 0]}
 if {[info exists ying] && [info exists yang]} {
         if {[string match $ying $yang]} { return true } else { return false } } else { return false }
+}
+
+proc youtubehandler {url test} {
+set urlfound false
+if {[lindex [split [lindex [split $url "//"] 2] .] end-1] == "youtube"} {
+        foreach item [split [lindex [split $url "/"] end] "&"] {
+                if {[string match [lindex [split [lindex [split $item "="] 0] "?"] 0] watch]} {
+                        set ying [lindex [split [lindex [split $item "="] 1] "?"] 0] } }
+}
+if {[lindex [split [lindex [split $test "//"] 2] .] end-1] == "youtube"} {
+        foreach item [split [lindex [split $test "/"] end] "&"] {
+                if {[string match [lindex [split [lindex [split $item "="] 0] "?"] 0] watch]} {
+                        set yang [lindex [split [lindex [split $item "="] 1] "?"] 0] } }
+}
+
+if {[lindex [split [lindex [split $url "//"] 2] .] end-1] == "youtu"} { set ying [lindex [split $url "/"] end] }
+if {[lindex [split [lindex [split $test "//"] 2] .] end-1] == "youtu"} { set yang [lindex [split $test "/"] end] }
+
+if {[info exists ying] && [info exists yang]} {
+        if {[string match $ying $yang]} {return true } else {return false} } else {return false }
 }
 
 }
